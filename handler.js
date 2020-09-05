be749bb9764adf38ea9bf2748a039153a895acfa
Goi Jia Jian <jiajiannn@gmail.com>
2020-09-05 11:24:00 +0800

    feat: SC Equities parser, dynamodb insert

--- handler.js
+++ handler.js
@@ -1,15 +1,17 @@
 'use strict';
 
 const AWS = require('aws-sdk');
+const { v4: uuidv4 } = require('uuid');
+const simpleParser = require('mailparser').simpleParser;
 
 const s3 = new AWS.S3({
   apiVersion: '2006-03-01',
   region: process.env.AWSREGION,
 });
 
-const simpleParser = require('mailparser').simpleParser;
+var docClient = new AWS.DynamoDB.DocumentClient();
 
-module.exports.postprocess = async (event) => {
+module.exports.postprocess = async (event, context) => {
   // console.log('Received event:', JSON.stringify(event, null, 2));
   const record = event.Records[0];
   // Retrieve the email from your bucket
@@ -22,13 +24,113 @@ module.exports.postprocess = async (event) => {
     const data = await s3.getObject(request).promise();
     // console.log('Raw email:' + data.Body);
     const email = await simpleParser(data.Body);
-    console.log(email);
+    const senderAddress = email.from.value[0].address;
     console.log({
-      from: email.from.text,
+      from: senderAddress,
       subject: email.subject,
       date: email.date,
       body: email.text,
     });
+
+    // SC
+    const subject = email.subject;
+    const body = email.text;
+    if (subject.includes("SCBSL")) {
+      if (body.includes("Order Filled")) {
+        let orderSide = "";
+        let stockExchange = "";
+        let stockCode = "";
+        let currency = "";
+        let price = 0;
+        let quantity = 0;
+
+        const bodySplit = body.split("\n");
+
+        String.prototype.indexOfEndPosition = function (string) {
+          var io = this.indexOf(string);
+          return io == -1 ? -1 : io + string.length;
+        }
+
+        bodySplit.forEach(line => {
+          const orderSideText = "Order Side: ";
+          if (line.includes(orderSideText)) {
+            orderSide = line.substring(
+              line.indexOfEndPosition(orderSideText),
+              line.length
+            );
+          }
+          const stockExchangeText = "Stock Exchange: ";
+          if (line.includes(stockExchangeText)) {
+            stockExchange = line.substring(
+              line.indexOfEndPosition(stockExchangeText),
+              line.length
+            );
+          }
+          const stockCodeText = "Stock Code: ";
+          if (line.includes(stockCodeText)) {
+            stockCode = line.substring(
+              line.indexOfEndPosition(stockCodeText),
+              line.length
+            );
+          }
+
+          const priceText = "Filled Price: ";
+          if (line.includes(priceText)) {
+            const priceAndQuantity = line.substring(
+              line.indexOfEndPosition(priceText),
+              line.length
+            );
+            [currency, price] = priceAndQuantity.split(" "); // Example format: SGD 0.635
+          }
+          const quantityText = "Filled Quantity: ";
+          if (line.includes(quantityText)) {
+            quantity = line.substring(
+              line.indexOfEndPosition(quantityText),
+              line.lastIndexOf(" shares")
+            );
+          }
+        });
+        const uuid = uuidv4();
+        var portfolioRecord = {
+          TableName: 'gs_portfolio',
+          ReturnConsumedCapacity: 'TOTAL',
+          Item: {
+            'id': uuid,
+            'email': senderAddress,
+            'date': email.date.toISOString(),
+            'orderSide': orderSide,
+            'stockExchange': stockExchange,
+            'stockCode': stockCode,
+            'currency': currency,
+            'price': price,
+            'quantity': quantity,
+          }
+        };
+        // Call DynamoDB to add the item to the table
+        await docClient.put(portfolioRecord).promise().then(
+          function (data) {
+            console.log("DynamoDB success", data);
+          },
+          function (error) {
+            console.log("DynamoDB error", error);
+          }
+        );
+
+        const result = {
+          id: uuid,
+          email: senderAddress,
+          orderSide,
+          stockExchange,
+          stockCode,
+          currency,
+          price,
+          quantity,
+        };
+        console.log(result);
+        return { status: 'success', result };
+      }
+    }
+
     return { status: 'success' };
   } catch (Error) {
     console.log(Error, Error.stack);
